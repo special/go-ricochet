13b2f9111d8a1f50aa2b70bd23700550da360654
Sarah Jamie Lewis <me@sarahjamielewis.com>
2016-11-08 14:42:58 -0800

    Counting main package in tests

--- tests.sh
+++ tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 pwd
-go test -coverprofile=cover.out -v .
+go test -coverprofile=main.cover.out -v .
 go test -coverprofile=utils.cover.out -v ./utils
 echo "mode: set" > coverage.out && cat *.cover.out | grep -v mode: | sort -r | \
 awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
