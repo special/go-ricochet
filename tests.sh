cfc7dc243cee56b830fbb0811afc9793c088fa95
Sarah Jamie Lewis <me@sarahjamielewis.com>
2016-11-08 14:10:52 -0800

    Add script for coveralls

---
+++ tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+pwd
+go test -coverprofile=cover.out -v .
+go test -coverprofile=utils.cover.out -v ./utils
+echo "mode: set" > coverage.out && cat *.cover.out | grep -v mode: | sort -r | \
+awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
+rm -rf *.cover.out
